2e37c0103cffb5ed9912af27ed22bf0ba516bafd
Davyd <dvolosh@unc.edu>
2026-02-13 12:52:38 -0500

    ...

--- firm_selection/reit_script.py
+++ firm_selection/reit_script.py
@@ -3,6 +3,7 @@ import numpy as np
 
 df = pd.read_csv("compustat_reit_firms.csv", dtype=str)
 res = df['cusip'].unique()
-cusip_string = " ".join(res.astype(str))
+
+cusip_string = "\n".join(res.astype(str))
 with open("reit_cusips.txt", "w") as f:
     f.write(cusip_string)
